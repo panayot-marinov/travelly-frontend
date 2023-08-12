66063a876eaa1cd76daad84bc2db43bc7d11ec20
psm2001 <panayot.marinov12@gmail.com>
2023-08-12 11:56:04 +0300

    Created navbar via Material

---
+++ src/app/timeline/timeline.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+@Component({
+  selector: 'app-timeline',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  templateUrl: './timeline.component.html',
+  styleUrls: ['./timeline.component.css'],
+})
+export class TimelineComponent {
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
+
+  constructor(private _formBuilder: FormBuilder) {}
+}
