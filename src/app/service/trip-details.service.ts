90d8c398656f5c68efabcd5f666a9e6220fc42ad
psm2001 <panayot.marinov12@gmail.com>
2023-08-20 17:02:25 +0300

    Fixed merge conflict

--- src/app/service/trip-details.service.ts
+++ src/app/service/trip-details.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
 import {TripListModel} from "../model/trip-list.model";
 import {TripDetailsModel} from "../model/trip-details.model";
+import {Observable} from "rxjs";
+import {Trip} from "../model/trip.model";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TripDetailsService {
-  private trip: TripDetailsModel = { id: 1, name: 'aaa', destination: 'Paris' }
+  // private trip: TripDetailsModel = { id: 1, name: 'aaa', destination: 'Paris' }
 
-  public getTrip(): TripDetailsModel {
-    return this.trip;
-  }
+  // public getTrip(): TripDetailsModel {
+  //   return this.trip;
+  // }
 
   // public getTripById(id: number): TripDetailsModel | undefined {
   //   return this.trips.find(trip => trip.id === id);
   // }
 
-  constructor() { }
+  private readonly apiUrl: string = 'http://localhost:8080';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getTripById(id: number): Observable<Trip> {
+    console.log('Searching for a trip with id ', id);
+    return this.httpClient.get<Trip>(this.apiUrl + '/trips/' + id);
+  }
 }
--- src/app/service/trip-list.service.ts
+++ src/app/service/trip-list.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
 import {TripListModel} from "../model/trip-list.model";
+import {Observable} from "rxjs";
+import {Trip} from "../model/trip.model";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TripListService {
-  private trips: TripListModel[] = [
-    { id: 1, name: 'aaa', destination: 'Paris' },
-    { id: 2, name: 'bbb', destination: 'New York'}
-  ]
+  private readonly apiUrl: string = 'http://localhost:8080';
 
-  public getTrips(): TripListModel[] {
-    return this.trips;
+  // private trips: TripListModel[] = [
+  //   { id: 1, name: 'aaa', destination: 'Paris' },
+  //   { id: 2, name: 'bbb', destination: 'New York'}
+  // ]
+  //
+  // public getTrips(): TripListModel[] {
+  //   return this.trips;
+  // }
+
+  constructor(private httpClient:HttpClient) {
+    // this.apiUrl = apiUrl;
   }
 
-  constructor() { }
+  public getTrips(): Observable<Trip[]> {
+    return this.httpClient.get<Trip[]>(this.apiUrl + '/trips')
+  }
+
+  public getTripById(id: number): Observable<Trip> {
+    return this.httpClient.get<Trip>(this.apiUrl + '/trips/' + id)
+  }
 }
