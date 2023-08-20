bdea5296bc6a141b1a742cad0a3aac72bbe62174
psm2001 <panayot.marinov12@gmail.com>
2023-08-20 14:09:19 +0300

    Commented some model fields

--- src/app/model/trip-details.model.ts
+++ src/app/model/trip-details.model.ts
@@ -5,25 +5,25 @@ export class TripDetailsModel {
   id: number;
   name: string;
   destination: string;
-  startDate: Date;
-  endDate: Date;
-  budget: number;
-  interests: string;
-  items: Item[]= [];
-  users: User[] = [];
+  // startDate: Date;
+  // endDate: Date;
+  // budget: number;
+  // interests: string;
+  // items: Item[]= [];
+  // users: User[] = [];
 
   constructor(id: number, name: string, destination: string,
-              startDate: Date, endDate: Date, budget: number,
-              interests: string, items: Item[], users: User[]
+              // startDate: Date, endDate: Date, budget: number,
+              // interests: string, items: Item[], users: User[]
   ) {
     this.id = id;
     this.name = name;
     this.destination = destination;
-    this.startDate = startDate;
-    this.endDate = endDate;
-    this.budget = budget;
-    this.interests = interests;
-    this.items = items;
-    this.users = users;
+    // this.startDate = startDate;
+    // this.endDate = endDate;
+    // this.budget = budget;
+    // this.interests = interests;
+    // this.items = items;
+    // this.users = users;
   }
 }
