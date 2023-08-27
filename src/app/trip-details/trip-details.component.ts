2e613ae3841e21133328000c1cbc1ec6e90750a1
psm2001 <panayot.marinov12@gmail.com>
2023-08-27 20:12:21 +0300

    Added openEditTripDialog() method

--- src/app/trip-details/trip-details.component.ts
+++ src/app/trip-details/trip-details.component.ts
@@ -1,7 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {TripDetailsModel} from "../model/trip-details.model";
-import {TripDetailsService} from "../service/trip-details.service";
-import {ActivatedRoute, Router} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TripDetailsModel } from '../model/trip-details.model';
+import { TripDetailsService } from '../service/trip-details.service';
+import { MatDialog } from '@angular/material/dialog';
+import { EditTripDialogComponent } from '../edit-trip-dialog/edit-trip-dialog.component';
 
 @Component({
   selector: 'app-trip-details',
@@ -14,13 +16,14 @@ export class TripDetailsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private tripService: TripDetailsService
+    private tripService: TripDetailsService,
+    private dialog: MatDialog
   ) {}
 
-  public ngOnInit(): void {
+  ngOnInit(): void {
     const tripIdParam = this.route.snapshot.paramMap.get('id');
 
-    if(tripIdParam != null) {
+    if (tripIdParam != null) {
       const tripId = +tripIdParam;
       this.tripService.getTripById(tripId).subscribe(
         (data) => {
@@ -29,12 +32,26 @@ export class TripDetailsComponent implements OnInit {
         (error) => {
           console.error('Cannot fetch trip by id: ', error);
         }
-      )
+      );
     }
   }
 
-  public loadTrips(): void {
-    this.router.navigate(['/trips']);
+  openEditTripDialog(): void {
+    const dialogRef = this.dialog.open(EditTripDialogComponent, {
+      width: '400px', // Adjust the width as needed
+      data: this.trip // Pass the current trip data to the dialog
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // Handle dialog close event, if needed
+      if (result) {
+        // Update the trip data if the dialog was closed with changes
+        this.trip = result;
+      }
+    });
   }
 
+  loadTrips(): void {
+    this.router.navigate(['/trips']);
+  }
 }
