374463b6076f478c7b795cc08ce14d9d3b6a6712
psm2001 <panayot.marinov12@gmail.com>
2023-08-28 20:39:02 +0300

    add ItemListComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { LoginComponent } from './login/login.component';
 import {FormsModule} from "@angular/forms";
 import {RegistrationComponent} from "./registration/registration.component";
 import {MatFormFieldModule} from "@angular/material/form-field";
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import {MatFormFieldModule} from "@angular/material/form-field";
     TripDetailsComponent,
     LoginComponent,
     RegistrationComponent,
+    ItemListComponent,
   ],
   imports: [
     BrowserModule,
