d39390bac4ddf2bc628085636be8b3f72e64f1f3
psm2001 <panayot.marinov12@gmail.com>
2023-08-28 20:39:12 +0300

    add ItemListComponent

---
+++ src/app/item-list/item-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Item } from '../model/item.model'; // Make sure to import your Item model
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.css']
+})
+export class ItemListComponent {
+  @Input() items: Item[] = []; // Input property to receive the list of items
+}
