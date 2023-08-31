617bbafcd1c540bd182aaeee2efb03417857f76b
tuechki <dtuechki@gmail.com>
2023-08-31 15:28:21 +0300

    Update auth service calling the API.

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {UserRegistration} from "../model/user-registration.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private readonly apiUrl: string = 'http://localhost.8080';
+  private readonly apiUrl: string = 'http://localhost:8080/users';
 
   constructor(private http: HttpClient) {}
 
-  register (username: string, password: string): Observable<any> {
-    const body = { username, password };
-    return this.http.post(this.apiUrl + '/register', body);
+  register(userRegistration: UserRegistration): Observable<any> {
+    console.log(userRegistration)
+    return this.http.post(`${this.apiUrl}/register`, userRegistration);
   }
 
   login (username: string, password: string): Observable<any> {
