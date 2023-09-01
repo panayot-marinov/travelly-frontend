b54e2f9306c59a2455a5b0779100b93be2a04d49
tuechki <dtuechki@gmail.com>
2023-09-01 13:31:11 +0300

    Set trips using TripDataService.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {AuthService} from "../service/auth.service";
 import {Router} from "@angular/router";
 import {UserLogin} from "../model/user-login.model";
+import {TripList} from "../model/trip-list.model";
+import {TripDataService} from "../service/trip-data.service";
 
 @Component({
   selector: 'app-login',
@@ -14,15 +16,18 @@ export class LoginComponent {
     password: '',
   };
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private tripDataService: TripDataService, private router: Router) {}
 
   onSubmit() {
-    this.authService.register(this.userLogin).subscribe(
+    this.authService.login(this.userLogin).subscribe(
       (response) => {
-        this.router.navigate(['/trips']);
-      },
-      (error) => {
-        // Handle registration error
+        const userId = response['userId'];
+        this.authService.getTripsByUserId(userId).subscribe(
+          (trips: TripList[]) => {
+            this.tripDataService.setTrips(trips);
+            this.router.navigate(['trips'], { state: { trips } });
+          }
+        );
       }
     );
   }
