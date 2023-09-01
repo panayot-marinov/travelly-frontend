2a661eebdc0a9e47fc6d5a5677e138d86882889c
tuechki <dtuechki@gmail.com>
2023-09-02 00:08:09 +0300

    Add TripViewEdit to routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {TripListComponent} from "./trip-list/trip-list.component";
 import {RegistrationComponent} from "./registration/registration.component";
 import {LoginComponent} from "./login/login.component";
-import {TripViewComponent} from "./trip-view/trip-view.component";
+import {TripViewEditComponent} from "./trip-view/trip-view-edit.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/trips', pathMatch: 'full' },
   { path: 'trips', component: TripListComponent },
-  { path: 'trips/:tripId', component: TripViewComponent },
+  { path: 'trips/:tripId', component: TripViewEditComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent }
 ];
