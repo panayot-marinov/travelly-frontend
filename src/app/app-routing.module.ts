2ae901cbfe695919d7383a198e4f9076b7da4b11
tuechki <dtuechki@gmail.com>
2023-09-02 01:47:50 +0300

    Add component contact to routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,17 @@ import {LoginComponent} from "./login/login.component";
 import {TripViewEditComponent} from "./trip-view-edit/trip-view-edit.component";
 import {TripCreateComponent} from "./trip-create/trip-create.component";
 import {AboutComponent} from "./about/about.component";
+import {ContactComponent} from "./contact/contact.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '', redirectTo: '/trips', pathMatch: 'full' },
   { path: 'trips', component: TripListComponent },
   { path: 'trips/:tripId', component: TripViewEditComponent },
-  { path: 'users/:userId/trips', component: TripCreateComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'users/:userId/trips', component: TripCreateComponent }
 
 ];
 
