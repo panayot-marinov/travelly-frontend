a81b1fb0d0c905f0eb6bf6a2c3b649dd2597fa61
tuechki <dtuechki@gmail.com>
2023-09-01 17:53:31 +0300

    Change number array to date.

--- src/app/model/trip-list.model.ts
+++ src/app/model/trip-list.model.ts
@@ -1,14 +1,11 @@
-import {Item} from "./item.model";
-import {User} from "./user.model";
-
 export class TripList {
   id: number;
   name: string;
   destination: string;
-  startDate: number[];
-  endDate: number[];
+  startDate: Date;
+  endDate: Date;
 
-  constructor(id: number, name: string, destination: string, startDate: number[], endDate: number[]) {
+  constructor(id: number, name: string, destination: string, startDate: Date, endDate: Date) {
     this.id = id;
     this.name = name;
     this.destination = destination;
