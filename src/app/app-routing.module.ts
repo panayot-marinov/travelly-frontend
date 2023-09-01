7db0dfc5d7470cb182b0a0c37468739898b131b2
tuechki <dtuechki@gmail.com>
2023-09-02 00:02:41 +0300

    Add routing for TripViewComponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {TripListComponent} from "./trip-list/trip-list.component";
-import {TripDetailsComponent} from "./trip-details/trip-details.component";
 import {RegistrationComponent} from "./registration/registration.component";
 import {LoginComponent} from "./login/login.component";
+import {TripViewComponent} from "./trip-view/trip-view.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/trips', pathMatch: 'full' },
   { path: 'trips', component: TripListComponent },
-  { path: 'trips/:id', component: TripDetailsComponent },
+  { path: 'trips/:tripId', component: TripViewComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent }
 ];
