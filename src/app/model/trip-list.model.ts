bed0527db8e35115ee0ee59668f9bbbab5eacef2
tuechki <dtuechki@gmail.com>
2023-09-01 13:32:18 +0300

    Add startDate and endDate to TripList

--- src/app/model/trip-list.model.ts
+++ src/app/model/trip-list.model.ts
@@ -1,20 +1,18 @@
 import {Item} from "./item.model";
 import {User} from "./user.model";
 
-export class TripListModel {
+export class TripList {
   id: number;
   name: string;
   destination: string;
-  // startDate: Date;
-  // endDate: Date;
+  startDate: number[];
+  endDate: number[];
 
-  constructor(id: number, name: string, destination: string,
-                //string, startDate: Date, endDate: Date
-) {
+  constructor(id: number, name: string, destination: string, startDate: number[], endDate: number[]) {
     this.id = id;
     this.name = name;
     this.destination = destination;
-    // this.startDate = startDate;
-    // this.endDate = endDate;
+    this.startDate = startDate;
+    this.endDate = endDate;
   }
 }
