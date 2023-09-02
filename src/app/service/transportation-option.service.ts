bf6dae37bb0b5b19dd2199dfcbdea107dd32fbf5
tuechki <dtuechki@gmail.com>
2023-09-02 23:10:17 +0300

    Add Transportation service with basic add, get, delete.

---
+++ src/app/service/transportation-option.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {TransportationOption} from "../model/transportation-option.model";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransportationOptionService {
+  private apiUrl = 'http://localhost:8080/transportationOption';
+
+  constructor(private http: HttpClient) {}
+
+  deleteTransportationOptionById (transportationOptionId: number): Observable<any> {
+    const url = `${this.apiUrl}/${transportationOptionId}`;
+    return this.http.delete<void>(url);
+  }
+
+  getTransportationOptionById(transportationOptionId: number): Observable<TransportationOption> {
+    const url = `${this.apiUrl}/${transportationOptionId}`
+    return this.http.get<TransportationOption>(url);
+  }
+
+  updateTransportationOption(transportationOption: TransportationOption): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/${transportationOption.id}`, transportationOption);
+  }
+
+}
