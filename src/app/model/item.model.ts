a17f1ab94d71c6c95dd1359c335afcdbba21f208
tuechki <dtuechki@gmail.com>
2023-09-03 11:39:04 +0300

    Change item class to interface and update fields.

--- src/app/model/item.model.ts
+++ src/app/model/item.model.ts
@@ -1,20 +1,7 @@
-import {Trip} from "./trip.model";
-
-export class Item {
+export interface Item {
   id: number;
-  trip: Trip;
   name: string;
-  desc: string;
+  description: string;
   amount: number;
   isPacked: boolean;
-
-  constructor(id: number, trip: Trip, name: string, desc: string, amount: number, isPacked: boolean) {
-    this.id = id;
-    this.trip = trip;
-    this.name = name;
-    this.desc = desc;
-    this.amount = amount;
-    this.isPacked = isPacked;
-  }
-
 }
